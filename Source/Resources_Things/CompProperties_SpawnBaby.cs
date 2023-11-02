8fdf0b8d2d9f7615308c29f70bfec45fc5c029d1
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2023-11-03 01:59:38 +0300

    v1.15.18_SmallFix

--- Source/Resources_Things/CompProperties_SpawnBaby.cs
+++ Source/Resources_Things/CompProperties_SpawnBaby.cs
@@ -1,161 +1,184 @@
 using RimWorld;
 using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace WVC_XenotypesAndGenes
 {
 
-    public class CompProperties_SpawnBabyPawnAndInheritGenes : CompProperties
-    {
-        public IntRange ticksBetweenSpawn = new(60000, 120000);
+	public class CompProperties_SpawnBabyPawnAndInheritGenes : CompProperties
+	{
+		public IntRange ticksBetweenSpawn = new(60000, 120000);
 
-        public GeneDef geneDef = null;
+		public GeneDef geneDef = null;
 
-        public string uniqueTag = "ResurgentChild";
+		public string uniqueTag = "ResurgentChild";
 
-        public string completeMessage = "WVC_RB_Gene_MechaGestator";
+		public string completeMessage = "WVC_RB_Gene_MechaGestator";
 
-        public CompProperties_SpawnBabyPawnAndInheritGenes()
-        {
-            compClass = typeof(CompSpawnBabyPawnAndInheritGenes);
-        }
-    }
+		public CompProperties_SpawnBabyPawnAndInheritGenes()
+		{
+			compClass = typeof(CompSpawnBabyPawnAndInheritGenes);
+		}
+	}
 
-    public class CompSpawnBabyPawnAndInheritGenes : ThingComp
-    {
-        public int tickCounter = 0;
+	public class CompSpawnBabyPawnAndInheritGenes : ThingComp
+	{
+		public int tickCounter = 0;
 
-        private Pawn childOwner = null;
+		private Pawn childOwner = null;
 
-        // private int ticksBetweenSpawn = 0;
+		private bool spawnerIsActive = true;
 
-        private CompProperties_SpawnBabyPawnAndInheritGenes Props => (CompProperties_SpawnBabyPawnAndInheritGenes)props;
+		// private int ticksBetweenSpawn = 0;
 
-        public override void Initialize(CompProperties props)
-        {
-            base.Initialize(props);
-            ResetCounter();
-            // GetChildOwner();
-        }
+		private CompProperties_SpawnBabyPawnAndInheritGenes Props => (CompProperties_SpawnBabyPawnAndInheritGenes)props;
 
-        public override void PostSpawnSetup(bool respawningAfterLoad)
-        {
-            base.PostSpawnSetup(respawningAfterLoad);
-            if (!respawningAfterLoad)
-            {
-                ResetCounter();
-            }
-            GetChildOwner();
-        }
+		public override void Initialize(CompProperties props)
+		{
+			base.Initialize(props);
+			ResetCounter();
+			// GetChildOwner();
+		}
 
-        public override void CompTick()
-        {
-            base.CompTick();
-            Tick(1);
-        }
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			base.PostSpawnSetup(respawningAfterLoad);
+			if (!respawningAfterLoad)
+			{
+				ResetCounter();
+			}
+			GetChildOwner();
+		}
 
-        public override void CompTickRare()
-        {
-            base.CompTickRare();
-            Tick(250);
-        }
+		public override void CompTick()
+		{
+			base.CompTick();
+			Tick(1);
+		}
 
-        public override void CompTickLong()
-        {
-            base.CompTickRare();
-            Tick(2000);
-        }
+		public override void CompTickRare()
+		{
+			base.CompTickRare();
+			Tick(250);
+		}
 
-        public void Tick(int tick)
-        {
-            tickCounter -= tick;
-            if (tickCounter > 0)
-            {
-                return;
-            }
-            if (childOwner == null)
-            {
-                ForceChildOwner();
-            }
-            TryDoSpawn();
-            ResetCounter();
-            GetChildOwner();
-        }
+		public override void CompTickLong()
+		{
+			base.CompTickRare();
+			Tick(2000);
+		}
 
-        public void TryDoSpawn()
-        {
-            if (childOwner != null)
-            {
-                GestationUtility.GenerateNewBornPawn_Thing(childOwner, Props.completeMessage, true, true, parent);
-            }
-        }
+		public void Tick(int tick)
+		{
+			tickCounter -= tick;
+			if (tickCounter > 0)
+			{
+				return;
+			}
+			if (childOwner == null)
+			{
+				ForceChildOwner();
+			}
+			TryDoSpawn();
+			ResetCounter();
+			GetChildOwner();
+		}
 
-        public override string CompInspectStringExtra()
-        {
-            if (childOwner != null)
-            {
-                return "WVC_XaG_Label_CompSpawnBabyPawnAndInheritGenes".Translate((tickCounter).ToStringTicksToPeriod()) + "\n" + "WVC_XaG_Label_CompSpawnBabyPawnAndInheritGenes_ChildOwner".Translate() + ": " + childOwner.Name;
-            }
-            return null;
-        }
+		public void TryDoSpawn()
+		{
+			if (childOwner != null && spawnerIsActive)
+			{
+				GestationUtility.GenerateNewBornPawn_Thing(childOwner, Props.completeMessage, true, true, parent);
+			}
+		}
 
-        public override IEnumerable<Gizmo> CompGetGizmosExtra()
-        {
-            if (DebugSettings.ShowDevGizmos)
-            {
-                Command_Action command_Action = new()
-                {
-                    defaultLabel = "DEV: Spawn baby",
-                    action = delegate
-                    {
-                        ResetCounter();
-                        TryDoSpawn();
-                        GetChildOwner();
-                    }
-                };
-                yield return command_Action;
-            }
-        }
+		public override string CompInspectStringExtra()
+		{
+			if (childOwner != null && spawnerIsActive)
+			{
+				return "WVC_XaG_Label_CompSpawnBabyPawnAndInheritGenes".Translate((tickCounter).ToStringTicksToPeriod()) + "\n" + "WVC_XaG_Label_CompSpawnBabyPawnAndInheritGenes_ChildOwner".Translate() + ": " + childOwner.Name;
+			}
+			return null;
+		}
 
-        public void ResetCounter()
-        {
-            tickCounter = Props.ticksBetweenSpawn.RandomInRange;
-        }
+		public override IEnumerable<Gizmo> CompGetGizmosExtra()
+		{
+			if (DebugSettings.ShowDevGizmos)
+			{
+				yield return new Command_Action
+				{
+					defaultLabel = "DEV: Spawn baby",
+					action = delegate
+					{
+						ResetCounter();
+						TryDoSpawn();
+						GetChildOwner();
+					}
+				};
+			}
+			yield return new Command_Action
+			{
+				defaultLabel = "WVC_XaG_GeneBabyTree_label".Translate() + ": " + OnOff(),
+				defaultDesc = "WVC_XaG_GeneBabyTree_desc".Translate(),
+				// icon = ContentFinder<Texture2D>.Get(parent.def.uiIcon),
+				icon = parent.def.uiIcon,
+				action = delegate
+				{
+					spawnerIsActive = !spawnerIsActive;
+				}
+			};
+		}
 
-        public void GetChildOwner()
-        {
-            if (childOwner == null)
-            {
-                ForceChildOwner();
-            }
-        }
+		private string OnOff()
+		{
+			if (spawnerIsActive)
+			{
+				return "WVC_XaG_Gene_DustMechlink_On".Translate();
+			}
+			return "WVC_XaG_Gene_DustMechlink_Off".Translate();
+		}
 
-        public void ForceChildOwner()
-        {
-            List<Pawn> possibleParents = new();
-            List<Pawn> list = parent.Map.mapPawns.FreeAdultColonistsSpawned;
-            foreach (Pawn item in list)
-            {
-                // if (item.Faction == Faction.OfPlayer)
-                // {
-                // }
-                if (MechanoidizationUtility.HasActiveGene(Props.geneDef, item) || Props.geneDef == null)
-                {
-                    possibleParents.Add(item);
-                }
-            }
-            if (possibleParents != null && possibleParents.Count > 0)
-            {
-                childOwner = possibleParents.RandomElement();
-            }
-        }
+		public void ResetCounter()
+		{
+			tickCounter = Props.ticksBetweenSpawn.RandomInRange;
+		}
 
-        public override void PostExposeData()
-        {
-            base.PostExposeData();
-            Scribe_Values.Look(ref tickCounter, "tickCounterNextBabySpawn_" + Props.uniqueTag, 0);
-            Scribe_References.Look(ref childOwner, "childOwner");
-        }
-    }
+		public void GetChildOwner()
+		{
+			if (childOwner == null)
+			{
+				ForceChildOwner();
+			}
+		}
+
+		public void ForceChildOwner()
+		{
+			List<Pawn> possibleParents = new();
+			List<Pawn> list = parent.Map.mapPawns.FreeAdultColonistsSpawned;
+			foreach (Pawn item in list)
+			{
+				// if (item.Faction == Faction.OfPlayer)
+				// {
+				// }
+				if (MechanoidizationUtility.HasActiveGene(Props.geneDef, item) || Props.geneDef == null)
+				{
+					possibleParents.Add(item);
+				}
+			}
+			if (possibleParents != null && possibleParents.Count > 0)
+			{
+				childOwner = possibleParents.RandomElement();
+			}
+		}
+
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Values.Look(ref tickCounter, "tickCounterNextBabySpawn_" + Props.uniqueTag, 0);
+			Scribe_References.Look(ref childOwner, "childOwner");
+			Scribe_Values.Look(ref spawnerIsActive, "babySpawn_OnOff", true);
+		}
+	}
 
 }
