048d86c4e5741a051764901b521ec642fd0606d3
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2023-11-22 00:22:12 +0300

    SmallFix

--- Source/Resources_Hediff/HediffComp_DustGestator.cs
+++ Source/Resources_Hediff/HediffComp_DustGestator.cs
@@ -4,69 +4,78 @@ using Verse;
 namespace WVC_XenotypesAndGenes
 {
 
-    public class HediffCompProperties_ImmaculateConception : HediffCompProperties
-    {
-        public float requiredSeverity = 0.9f;
+	public class HediffCompProperties_ImmaculateConception : HediffCompProperties
+	{
+		public float requiredSeverity = 0.9f;
 
-        public float removeSeverity = 1.0f;
+		public float removeSeverity = 1.0f;
 
-        public string completeMessage = "WVC_XaG_Gene_ImmaculateConception";
+		public string completeMessage = "WVC_XaG_Gene_ImmaculateConception";
 
-        public bool endogeneTransfer = true;
+		public bool endogeneTransfer = true;
 
-        public bool xenogeneTransfer = true;
+		public bool xenogeneTransfer = true;
 
-        public HediffCompProperties_ImmaculateConception()
-        {
-            compClass = typeof(HediffComp_ImmaculateConception);
-        }
-    }
+		public HediffCompProperties_ImmaculateConception()
+		{
+			compClass = typeof(HediffComp_ImmaculateConception);
+		}
+	}
 
-    public class HediffComp_ImmaculateConception : HediffComp
-    {
-        public HediffCompProperties_ImmaculateConception Props => (HediffCompProperties_ImmaculateConception)props;
+	public class HediffComp_ImmaculateConception : HediffComp
+	{
+		public HediffCompProperties_ImmaculateConception Props => (HediffCompProperties_ImmaculateConception)props;
 
-        // protected int GestationIntervalDays => Props.gestationIntervalDays;
+		private bool canBirth = true;
 
-        // public override string CompLabelInBracketsExtra => GetLabel();
+		// protected int GestationIntervalDays => Props.gestationIntervalDays;
 
-        public override void CompPostTick(ref float severityAdjustment)
-        {
-            base.CompPostTick(ref severityAdjustment);
-            if (!Pawn.IsHashIntervalTick(1500))
-            {
-                return;
-            }
-            Pawn pawn = parent.pawn;
-            if (pawn.Map == null)
-            {
-                return;
-            }
-            if (pawn.Faction != Faction.OfPlayer)
-            {
-                base.Pawn.health.RemoveHediff(parent);
-                return;
-            }
-            if (parent.Severity >= Props.requiredSeverity)
-            {
-                GestationUtility.GenerateNewBornPawn(parent.pawn, Props.completeMessage, Props.endogeneTransfer, Props.xenogeneTransfer);
-            }
-            if (parent.Severity >= Props.removeSeverity)
-            {
-                base.Pawn.health.RemoveHediff(parent);
-            }
-        }
+		// public override string CompLabelInBracketsExtra => GetLabel();
 
-        // public string GetLabel()
-        // {
-        // Pawn pawn = parent.pawn;
-        // if (pawn.Faction == Faction.OfPlayer && pawn.ageTracker.CurLifeStage.reproductive)
-        // {
-        // float percent = (float)ticksCounter / (float)(ticksInday * GestationIntervalDays);
-        // return percent.ToStringPercent();
-        // }
-        // return "";
-        // }
-    }
+		public override void CompPostTick(ref float severityAdjustment)
+		{
+			base.CompPostTick(ref severityAdjustment);
+			if (!Pawn.IsHashIntervalTick(1500))
+			{
+				return;
+			}
+			Pawn pawn = parent.pawn;
+			if (pawn.Map == null)
+			{
+				return;
+			}
+			if (pawn.Faction != Faction.OfPlayer)
+			{
+				base.Pawn.health.RemoveHediff(parent);
+				return;
+			}
+			if (canBirth && parent.Severity >= Props.requiredSeverity)
+			{
+				canBirth = false;
+				GestationUtility.GenerateNewBornPawn(parent.pawn, Props.completeMessage, Props.endogeneTransfer, Props.xenogeneTransfer);
+			}
+			if (parent.Severity >= Props.removeSeverity)
+			{
+				base.Pawn.health.RemoveHediff(parent);
+			}
+		}
+
+		public override void CompExposeData()
+		{
+			base.CompExposeData();
+			Scribe_Values.Look(ref canBirth, "canBirth");
+		}
+
+		// public string GetLabel()
+		// {
+		// Pawn pawn = parent.pawn;
+		// if (pawn.Faction == Faction.OfPlayer && pawn.ageTracker.CurLifeStage.reproductive)
+		// {
+		// float percent = (float)ticksCounter / (float)(ticksInday * GestationIntervalDays);
+		// return percent.ToStringPercent();
+		// }
+		// return "";
+		// }
+	}
 
 }
