785b037c9f93b4b52c2c52f558921105b04bf306
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2023-12-07 14:25:04 +0300

    v1.33.12_Balance

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WVC_XenotypesAndGenes v1.33.06
+# WVC_XenotypesAndGenes v1.33.12
  
 Pre-release versions require resetting the mod settings.
 
--- Source/Resources_Things/CompSpawnOnDeath.cs
+++ Source/Resources_Things/CompSpawnOnDeath.cs
@@ -1,5 +1,6 @@
 using RimWorld;
 using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace WVC_XenotypesAndGenes
@@ -67,6 +68,74 @@ namespace WVC_XenotypesAndGenes
 			// FilthMaker.TryMakeFilth(pawn.Position, previousMap, Props.filthDefToSpawn, filthCount);
 			Thing thing = ThingMaker.MakeThing(Props.thingDefsToSpawn.RandomElement());
 			GenPlace.TryPlaceThing(thing, pawn.Position, previousMap, ThingPlaceMode.Near, null, null, default);
+		}
+
+	}
+
+	public class CompSpawnOnDeath_GetColor : ThingComp
+	{
+
+		public ThingDef rockDef;
+
+		private CompProperties_SpawnOnDeath Props => (CompProperties_SpawnOnDeath)props;
+
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			base.PostSpawnSetup(respawningAfterLoad);
+			if (respawningAfterLoad || rockDef == null)
+			{
+				Reset();
+			}
+		}
+
+		private void Reset()
+		{
+			rockDef = Props.thingDefsToSpawn.RandomElement();
+			LongEventHandler.ExecuteWhenFinished(Apply);
+		}
+
+		public override void PostDestroy(DestroyMode mode, Map previousMap)
+		{
+			base.PostDestroy(mode, previousMap);
+			Pawn pawn = parent as Pawn;
+			for (int i = 0; i < Props.filthCount; i++)
+			{
+				IntVec3 intVec = pawn.Position + GenRadial.RadialPattern[i];
+				FilthMaker.TryMakeFilth(intVec, previousMap, Props.filthDefToSpawn, 1);
+			}
+			Thing thing = ThingMaker.MakeThing(rockDef);
+			GenPlace.TryPlaceThing(thing, pawn.Position, previousMap, ThingPlaceMode.Near, null, null, default);
+		}
+
+		private void Apply()
+		{
+			if (parent is Pawn pawn)
+			{
+				PawnRenderer renderer = pawn.Drawer.renderer;
+				Color color = rockDef.graphic.data.color;
+				GraphicData graphicData = new();
+				graphicData.CopyFrom(pawn.ageTracker.CurKindLifeStage.bodyGraphicData);
+				graphicData.color = color;
+				graphicData.colorTwo = color;
+				if (!renderer.graphics.AllResolved)
+				{
+					renderer.graphics.ResolveAllGraphics();
+				}
+				renderer.graphics.nakedGraphic = graphicData.Graphic;
+				renderer.graphics.ClearCache();
+			}
+		}
+
+		public void SetStoneColour(ThingDef thingDef)
+		{
+			rockDef = thingDef;
+			LongEventHandler.ExecuteWhenFinished(Apply);
+		}
+
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Defs.Look(ref rockDef, "rockDef");
 		}
 
 	}
