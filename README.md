0055538b56b63e42b2ab5074a517e0aeb0475f10
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-01-16 21:17:57 +0300

    v1.48.87_Upd

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WVC_XenotypesAndGenes v1.48.85
+# WVC_XenotypesAndGenes v1.48.87
  
 Pre-release versions require resetting the mod settings.
 
--- Source/Resources_Main/WVC_GenesDefOf.cs
+++ Source/Resources_Main/WVC_GenesDefOf.cs
@@ -61,9 +61,13 @@ namespace WVC_XenotypesAndGenes
 		// Traits
 		// public static TraitDef WVC_XaG_ShapeshiftPhobia;
 		// History
+		[MayRequireIdeology]
 		public static HistoryEventDef WVC_ReimplanterResurrection;
+		[MayRequireIdeology]
 		public static HistoryEventDef WVC_UndeadResurrection;
+		[MayRequireIdeology]
 		public static HistoryEventDef WVC_Shapeshift;
+		[MayRequireIdeology]
 		public static HistoryEventDef WVC_XenotypeSerumUsed;
 	}
 
