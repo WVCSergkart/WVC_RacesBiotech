7c0913d750156453da59eb947f874560adefe178
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-05-12 10:29:22 +0300

    EmbryoOverrider

--- Source/Resources_Things/CompProperties_Genepack.cs
+++ Source/Resources_Things/CompProperties_Genepack.cs
@@ -21,9 +21,11 @@ namespace WVC_XenotypesAndGenes
 
 		public List<GeneDef> geneDefs;
 
+		public List<XenotypeDef> xenotypeDefs;
+
 		public List<XaG_CountWithChance> genepacks;
 
-		public string uniqueTag = "Backup";
+		public string uniqueTag = "XaG_GeneSetter";
 
 		public List<XaG_CountWithChance> genesCountProbabilities;
 		public RulePackDef genepackNamer;
@@ -95,5 +97,81 @@ namespace WVC_XenotypesAndGenes
 		// }
 
 	// }
+
+	public class CompHumanEmbryo : ThingComp
+	{
+
+		public CompProperties_Genepack Props => (CompProperties_Genepack)props;
+
+		// public override void PostPostMake()
+		// {
+			// OverrideGeneSet(Props.xenotypeDefs.RandomElement());
+		// }
+
+		// public bool overrided = false;
+
+		// public override string TransformLabel(string label)
+		// {
+			// if (overrided)
+			// {
+				// return label;
+			// }
+			// if (pawnSources.Count == 2)
+			// {
+				// return "ThingOfTwoSources".Translate(label, pawnSources[0].LabelShortCap, pawnSources[1].LabelShortCap);
+			// }
+			// return "ThingOfSource".Translate(label, pawnSources[0].LabelShortCap);
+		// }
+
+		public void OverrideGeneSet(XenotypeDef xenotypeDef = null)
+		{
+			if (parent is not HumanEmbryo embryo)
+			{
+				return;
+			}
+			if (!Props.xenotypeDefs.NullOrEmpty())
+			{
+				if (xenotypeDef == null)
+				{
+					xenotypeDef = Props.xenotypeDefs.RandomElement();
+				}
+				GeneSet geneSet = embryo.GeneSet;
+				for (int i = 0; i < geneSet.GenesListForReading.Count; i++)
+				{
+					GeneDef geneDef = geneSet.GenesListForReading[i];
+					geneSet.Debug_RemoveGene(geneDef);
+				}
+				foreach (GeneDef geneDef in xenotypeDef.genes)
+				{
+					geneSet.AddGene(geneDef);
+				}
+				geneSet.SortGenes();
+				geneSet.SetNameDirect(parent.def.label + " " + xenotypeDef.label);
+			}
+		}
+
+		public override IEnumerable<Gizmo> CompGetGizmosExtra()
+		{
+			if (DebugSettings.ShowDevGizmos)
+			{
+				Command_Action command_Action = new()
+				{
+					defaultLabel = "DEV: OverrideGeneSet",
+					action = delegate
+					{
+						OverrideGeneSet();
+					}
+				};
+				yield return command_Action;
+			}
+		}
+
+		// public override void PostExposeData()
+		// {
+			// base.PostExposeData();
+			// Scribe_Values.Look(ref overrided, "overrided_" + Props.uniqueTag, false);
+		// }
+
+	}
 
 }
