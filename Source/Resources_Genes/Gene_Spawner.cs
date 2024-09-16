6e4b5fd19a35eb22a9ffb4447ad94ab41c06bd99
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-09-16 19:39:40 +0300

    HorrorPlating

--- Source/Resources_Genes/Gene_Spawner.cs
+++ Source/Resources_Genes/Gene_Spawner.cs
@@ -457,4 +457,65 @@ namespace WVC_XenotypesAndGenes
 
 	}
 
+	public class Gene_HorrorPlating : Gene
+	{
+
+		public bool horrorSpawned = false;
+
+		public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+		{
+			if (!Active)
+			{
+				return;
+			}
+			if (ModsConfig.AnomalyActive && !horrorSpawned)
+			{
+				SpawnMetalhorrorWithoutHediff(pawn);
+				horrorSpawned = true;
+				MiscUtility.SpawnItems(pawn, ThingDefOf.Bioferrite, 42, false);
+			}
+		}
+
+		public static Pawn SpawnMetalhorrorWithoutHediff(Pawn infected)
+		{
+			if (!ModLister.CheckAnomaly("Metalhorror"))
+			{
+				return null;
+			}
+			Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(PawnKindDefOf.Metalhorror, Faction.OfEntities));
+			if (!GenAdj.TryFindRandomAdjacentCell8WayWithRoom(infected.SpawnedParentOrMe, out var result))
+			{
+				result = infected.PositionHeld;
+			}
+			CompMetalhorror compMetalhorror = pawn.TryGetComp<CompMetalhorror>();
+			compMetalhorror.emergedFrom = infected;
+			compMetalhorror.implantSource = null;
+			int index = 2;
+			int num = (int)infected.ageTracker.AgeBiologicalTicks / 2500;
+			if (num <= 24)
+			{
+				index = 0;
+			}
+			else if (num <= 72)
+			{
+				index = 1;
+			}
+			pawn.ageTracker.LockCurrentLifeStageIndex(index);
+			pawn.ageTracker.AgeBiologicalTicks = infected.ageTracker.AgeBiologicalTicks;
+			pawn.ageTracker.AgeChronologicalTicks = infected.ageTracker.AgeChronologicalTicks;
+			Pawn pawn2 = (Pawn)GenSpawn.Spawn(pawn, result, infected.MapHeld);
+			compMetalhorror.FindOrCreateEmergedLord();
+			Find.BattleLog.Add(new BattleLogEntry_Event(infected, RulePackDefOf.Event_MetalhorrorEmerged, pawn2));
+			pawn2.stances.stunner.StunFor(60, null, addBattleLog: false);
+			return pawn2;
+		}
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref horrorSpawned, "horrorSpawned", false);
+		}
+
+	}
+
 }
