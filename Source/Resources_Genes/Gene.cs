aa1a810fed32e98827076fcd820a9815945410f3
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-03-21 02:58:47 +0300

    upd

---
+++ Source/Resources_Genes/Gene.cs
@@ -0,0 +1,19 @@
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace WVC_XenotypesAndGenes
+{
+
+    public class Gene_A : Verse.Gene
+    {
+
+
+
+    }
+
+}
