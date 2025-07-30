a19339edfb6abcde6c30560f513dcfe4f412f992
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-07-30 09:32:10 +0300

    new fang implanter test

--- Source/Resources_Genes/Gene_Hemogen.cs
+++ Source/Resources_Genes/Gene_Hemogen.cs
@@ -11,8 +11,8 @@ using Verse.Sound;
 namespace WVC_XenotypesAndGenes
 {
 
-	// Rare Hemogen Drain
-	public class Gene_HemogenDependant : Gene
+    // Rare Hemogen Drain
+    public class Gene_HemogenDependant : Gene
 	{
 
 		[Unsaved(false)]
@@ -93,55 +93,6 @@ namespace WVC_XenotypesAndGenes
 		//	base.ExposeData();
 		//	Scribe_Values.Look(ref nextTick, "nextTick", -1);
 		//}
-
-	}
-
-	public class Gene_ImplanterFang : Gene_HemogenOffset, IGeneBloodfeeder
-	{
-
-		public GeneExtension_General General => def?.GetModExtension<GeneExtension_General>();
-
-		public void Notify_Bloodfeed(Pawn victim)
-		{
-			// Log.Error("0");
-			if (General == null || victim == null)
-			{
-				return;
-			}
-			if (victim.Dead)
-			{
-				return;
-			}
-			// Log.Error("1");
-			if (victim?.health?.immunity?.AnyGeneMakesFullyImmuneTo(MainDefOf.WVC_XaG_ImplanterFangsMark) == true)
-			{
-				return;
-			}
-			// Log.Error("2");
-			float? immunity = victim?.GetStatValue(StatDefOf.ImmunityGainSpeed, cacheStaleAfterTicks: 360000);
-			float chance = (General.reimplantChance / (immunity.HasValue && immunity.Value > 0.01f ? immunity.Value : 0.01f)) * WVC_Biotech.settings.hemogenic_ImplanterFangsChanceFactor;
-			if (!Rand.Chance(chance))
-			{
-				// Log.Error("2 Chance: " + (General.reimplantChance / (immunity > 0.01f ? immunity : 0.01f)).ToString());
-				return;
-			}
-			// Log.Error("3");
-			if (GeneUtility.PawnWouldDieFromReimplanting(pawn))
-			{
-				return;
-			}
-			// Log.Error("4");
-			if (ReimplanterUtility.TryReimplant(pawn, victim, General.reimplantEndogenes, General.reimplantXenogenes))
-			{
-				victim.health.AddHediff(MainDefOf.WVC_XaG_ImplanterFangsMark, ExecutionUtility.ExecuteCutPart(victim));
-				if (PawnUtility.ShouldSendNotificationAbout(pawn) || PawnUtility.ShouldSendNotificationAbout(victim))
-				{
-					int max = HediffDefOf.XenogerminationComa.CompProps<HediffCompProperties_Disappears>().disappearsAfterTicks.max;
-					int max2 = HediffDefOf.XenogermLossShock.CompProps<HediffCompProperties_Disappears>().disappearsAfterTicks.max;
-					Find.LetterStack.ReceiveLetter("LetterLabelGenesImplanted".Translate(), "LetterTextGenesImplanted".Translate(pawn.Named("CASTER"), victim.Named("TARGET"), max.ToStringTicksToPeriod().Named("COMADURATION"), max2.ToStringTicksToPeriod().Named("SHOCKDURATION")), LetterDefOf.NeutralEvent, new LookTargets(pawn, victim));
-				}
-			}
-		}
 
 	}
 
---
+++ Source/Resources_Genes/Gene_ImplanterFang.cs
@@ -0,0 +1,114 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace WVC_XenotypesAndGenes
+{
+    public class Gene_ImplanterFang : Gene_HemogenOffset, IGeneBloodfeeder
+	{
+
+		public GeneExtension_General General => def?.GetModExtension<GeneExtension_General>();
+
+		public virtual void Notify_Bloodfeed(Pawn victim)
+        {
+            // Log.Error("0");
+            if (General == null || victim == null)
+            {
+                return;
+            }
+            if (victim.Dead)
+            {
+                return;
+            }
+            // Log.Error("1");
+            if (victim?.health?.immunity?.AnyGeneMakesFullyImmuneTo(MainDefOf.WVC_XaG_ImplanterFangsMark) == true)
+            {
+                return;
+            }
+            // Log.Error("2");
+            float? immunity = victim?.GetStatValue(StatDefOf.ImmunityGainSpeed, cacheStaleAfterTicks: 360000);
+            float chance = (General.reimplantChance / (immunity.HasValue && immunity.Value > 0.01f ? immunity.Value : 0.01f)) * WVC_Biotech.settings.hemogenic_ImplanterFangsChanceFactor;
+            if (!Rand.Chance(chance))
+            {
+                // Log.Error("2 Chance: " + (General.reimplantChance / (immunity > 0.01f ? immunity : 0.01f)).ToString());
+                return;
+            }
+            // Log.Error("3");
+            if (GeneUtility.PawnWouldDieFromReimplanting(pawn))
+            {
+                return;
+            }
+            // Log.Error("4");
+            TryReimplant(victim);
+        }
+
+        public virtual void TryReimplant(Pawn victim)
+        {
+            if (ReimplanterUtility.TryReimplant(pawn, victim, General.reimplantEndogenes, General.reimplantXenogenes))
+            {
+                victim.health.AddHediff(MainDefOf.WVC_XaG_ImplanterFangsMark, ExecutionUtility.ExecuteCutPart(victim));
+                if (PawnUtility.ShouldSendNotificationAbout(pawn) || PawnUtility.ShouldSendNotificationAbout(victim))
+                {
+                    int max = HediffDefOf.XenogerminationComa.CompProps<HediffCompProperties_Disappears>().disappearsAfterTicks.max;
+                    int max2 = HediffDefOf.XenogermLossShock.CompProps<HediffCompProperties_Disappears>().disappearsAfterTicks.max;
+                    Find.LetterStack.ReceiveLetter("LetterLabelGenesImplanted".Translate(), "LetterTextGenesImplanted".Translate(pawn.Named("CASTER"), victim.Named("TARGET"), max.ToStringTicksToPeriod().Named("COMADURATION"), max2.ToStringTicksToPeriod().Named("SHOCKDURATION")), LetterDefOf.NeutralEvent, new LookTargets(pawn, victim));
+                }
+            }
+        }
+    }
+
+	public class Gene_ImplanterFang_NonRemove : Gene_ImplanterFang
+    {
+
+        public bool IsXenogene => pawn.genes.IsXenogene(this);
+
+        public override void TryReimplant(Pawn victim)
+        {
+            List<GeneDef> oldEndogenes = victim.genes.Endogenes.ConvertToDefs();
+            List<GeneDef> oldXenogenes = victim.genes.Xenogenes.ConvertToDefs();
+            bool isXenogene = IsXenogene;
+            if (ReimplanterUtility.TryReimplant(pawn, victim, !isXenogene, isXenogene))
+            {
+                if (isXenogene)
+                {
+                    foreach (GeneDef item in oldXenogenes)
+                    {
+                        if (!item.passOnDirectly)
+                        {
+                            continue;
+                        }
+                        if (!XaG_GeneUtility.HasGene(item, victim))
+                        {
+                            victim.genes.AddGene(item, false);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (GeneDef item in oldEndogenes)
+                    {
+                        if (!item.passOnDirectly)
+                        {
+                            continue;
+                        }
+                        if (!XaG_GeneUtility.HasGene(item, victim))
+                        {
+                            victim.genes.AddGene(item, true);
+                        }
+                    }
+                }
+                victim.health.AddHediff(MainDefOf.WVC_XaG_ImplanterFangsMark, ExecutionUtility.ExecuteCutPart(victim));
+                if (PawnUtility.ShouldSendNotificationAbout(pawn) || PawnUtility.ShouldSendNotificationAbout(victim))
+                {
+                    int max = HediffDefOf.XenogerminationComa.CompProps<HediffCompProperties_Disappears>().disappearsAfterTicks.max;
+                    int max2 = HediffDefOf.XenogermLossShock.CompProps<HediffCompProperties_Disappears>().disappearsAfterTicks.max;
+                    Find.LetterStack.ReceiveLetter("LetterLabelGenesImplanted".Translate(), "LetterTextGenesImplanted".Translate(pawn.Named("CASTER"), victim.Named("TARGET"), max.ToStringTicksToPeriod().Named("COMADURATION"), max2.ToStringTicksToPeriod().Named("SHOCKDURATION")), LetterDefOf.NeutralEvent, new LookTargets(pawn, victim));
+                }
+                ReimplanterUtility.TrySetSkinAndHairGenes(pawn);
+                ReimplanterUtility.PostImplantDebug(pawn);
+            }
+        }
+
+    }
+
+}
