e442db57e80f1ee1f1ec2ee27bf53ea847d2b582
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-08-09 10:58:00 +0300

    upd

---
+++ Source/Resources_Genes/Gene_DayNightSleepnes.cs
@@ -0,0 +1,47 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace WVC_XenotypesAndGenes
+{
+
+
+    public class Gene_DaySleep: Gene_AddOrRemoveHediff
+    {
+
+        public override void TickInterval(int delta)
+        {
+            base.TickInterval(delta);
+            if (pawn.IsHashIntervalTick(2671, delta))
+            {
+                SetRest();
+            }
+        }
+
+        public SimpleCurve curve = new()
+        {
+            new CurvePoint(0, 1f),
+            new CurvePoint(8, 0.6f),
+            new CurvePoint(11, 0.0f),
+            new CurvePoint(18, 0.0f),
+            new CurvePoint(19, 1f),
+            new CurvePoint(24f, 1f)
+        };
+
+        public void SetRest()
+        {
+            Map mapHeld = pawn.MapHeld;
+            if (mapHeld == null)
+            {
+                return;
+            }
+            Need_Rest needRest = pawn.needs?.rest;
+            if (needRest != null)
+            {
+                needRest.CurLevelPercentage = curve.Evaluate(GenLocalDate.HourOfDay(mapHeld.Tile));
+            }
+        }
+
+    }
+
+}
