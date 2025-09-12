8168748f678e1c913e9e58f3bcdd5d54abcd437d
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-09-12 15:12:34 +0300

    great family meme + thrall mutant flag clear

---
+++ Source/Resources_Genes/Gene_PerfectCanvas.cs
@@ -0,0 +1,20 @@
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace WVC_XenotypesAndGenes
+{
+
+    // Dev
+    public class Gene_PerfectCanvas : Gene_AddOrRemoveHediff
+    {
+
+
+
+    }
+
+}
