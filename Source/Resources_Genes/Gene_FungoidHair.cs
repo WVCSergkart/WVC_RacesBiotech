5dab5d3664a56c982602fd1507bbde596eff54e9
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-10-20 14:37:23 +0300

    fungoid hair
    for fungoid fork

---
+++ Source/Resources_Genes/Gene_FungoidHair.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace WVC_XenotypesAndGenes
+{
+    public class Gene_FungoidHair : Gene
+	{
+
+		public GeneExtension_Giver Props => def?.GetModExtension<GeneExtension_Giver>();
+
+		public Color color;
+
+		public override void PostAdd()
+		{
+			base.PostAdd();
+			if (Props != null && Props.holofaces.Where((GeneralHolder x) => x.visible).ToList().TryRandomElement(out GeneralHolder countWithChance))
+			{
+				SetColor(countWithChance.color);
+			}
+		}
+
+		public void SetColor(Color color)
+		{
+			this.color = color;
+			pawn?.Drawer?.renderer?.SetAllGraphicsDirty();
+		}
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref color, "color");
+		}
+
+	}
+
+}
